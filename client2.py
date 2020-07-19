842cefdfc85af5cc7a70dbf2a892d2df2808d6ac
Arnav Akula <akula.arnav@gmail.com>
2020-07-18 20:58:12 -0700

    Implemented pickles for client-shared object

--- client2.py
+++ client2.py
@@ -4,11 +4,10 @@ import pickle
 
 host = 'localhost'
 port = 5555
-lst = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-d = {1: 'hi', 2: 'there'}
+d = {1: 'how', 2: 'are', 3: 'you'}
 s.send(pickle.dumps(d))
 print(pickle.loads(s.recv(2048)))
--- server.py
+++ server.py
@@ -1,66 +1,37 @@
 import socket
+import pickle
 from _thread import *
-import sys
 
-def read_pos(s):
-    s = s.split(',')
-    return int(s[0]), int(s[1])
-
-def make_pos(tup):
-    return str(tup[0]) + ',' + str(tup[1])
-
-server = 'localhost'
+host = 'localhost'
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lst = []
 
-try:
-    s.bind((server, port))
-except socket.error as e:
-    print(str(e))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+print('Waiting for connection...')
 
 s.listen()
-print('Waiting for a connection, server started')
 
-#p1, p2
-pos = [(0, 0), (100, 100)]
-
-def threaded_client(conn, current_player):
-    #first, send start_pos
-    conn.send(str.encode(make_pos(pos[current_player])))
-
-    reply = ''
+def threaded_client(conn):
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            print('SERVER RECV DATA BELOW')
-            print(data)
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
 
             if not data:
-                print('Disconnected')
                 break
             else:
-                if current_player == 1:
-                    reply = pos[0]
-                else:
-                    reply = pos[1]
-
-                print('Received {}'.format(data))
-                print('Sending {}'.format(reply))
-                conn.sendall(str.encode(make_pos(reply)))
-
-            conn.sendall(str.encode(reply))
+                print('Received: {}'.format(data))
+                lst.append(data)
+                conn.send(pickle.dumps(lst))
         except:
             break
 
-            print('lost connection')
+            print('Lost connection')
             conn.close()
 
-current_player = 0
+#TODO implement some condition
 while True:
     conn, addr = s.accept()
-    print('Connected to {}'.format(addr))
-
-    start_new_thread(threaded_client, (conn, current_player))
-    current_player += 1
+    start_new_thread(threaded_client, (conn,))
